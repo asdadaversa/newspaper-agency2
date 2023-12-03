5705e8d9f813929fee5a441556234f5a0c503a4b
asdadaversa <peikqudakeus@gmail.com>
2023-12-03 16:49:45 +0200

    edit settings file

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@^$(@^19#%u(dfs&qc4ql4+146hnm637uk=q1anaz5c=#6ipvc"
+SECRET_KEY = "secret-key"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
